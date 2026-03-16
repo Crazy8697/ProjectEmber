2763d8413d3dfcaebc8c58ec9913a97a58938af0
Crazy8697 <dreher.adam@darkrelay.net>
2026-03-15 20:41:51 -0400

    fix: add missing RadioButton import in settings screen

--- android/app/src/main/java/com/projectember/mobile/ui/screens/SettingsScreen.kt
+++ android/app/src/main/java/com/projectember/mobile/ui/screens/SettingsScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
